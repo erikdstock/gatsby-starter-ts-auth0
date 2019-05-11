234ed8b2518c5549bde916383d1143557781b6c6
Erik Stockmeier <erik.d.stock@gmail.com>
2019-05-11 11:25:55 -0400

    setup typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage: React.FunctionComponent = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
